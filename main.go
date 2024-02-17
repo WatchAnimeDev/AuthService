cf331584dde4d595c6624662e97cad70454a536a
Shounak Ghosh <sghosh1810@gmail.com>
2024-02-17 18:50:30 +0530

    load dot env on local

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"auth-service/routers"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 	router := gin.Default()
 	router.GET("/uid/set", routers.SetUID)
